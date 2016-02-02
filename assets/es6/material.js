9f7f8280a6c63191eb2263f3708b1f87de484a11
Danilo Cuculic <danilo.cuculic@gmail.com>
2016-02-02 21:05:02 +0100

    Release 0.9.2: upgraded to Material Design Lite 1.1.0

--- assets/es6/material.js
+++ assets/es6/material.js
@@ -1,22 +1,24 @@
+
 // Component handler
-//= require ../vendor/foundation/src/mdlComponentHandler.js
+//= require ../vendor/material-design-lite/src/mdlComponentHandler.js
 // Polyfills/dependencies
-//= require ../vendor/foundation/src/third_party/**/*.js
+//= require ../vendor/material-design-lite/src/third_party/**/*.js
 // Base components
-//= require ../vendor/foundation/src/button/button.js
-//= require ../vendor/foundation/src/checkbox/checkbox.js
-//= require ../vendor/foundation/src/icon-toggle/icon-toggle.js
-//= require ../vendor/foundation/src/menu/menu.js
-//= require ../vendor/foundation/src/progress/progress.js
-//= require ../vendor/foundation/src/radio/radio.js
-//= require ../vendor/foundation/src/slider/slider.js
-//= require ../vendor/foundation/src/spinner/spinner.js
-//= require ../vendor/foundation/src/switch/switch.js
-//= require ../vendor/foundation/src/tabs/tabs.js
-//= require ../vendor/foundation/src/textfield/textfield.js
-//= require ../vendor/foundation/src/tooltip/tooltip.js
+//= require ../vendor/material-design-lite/src/button/button.js
+//= require ../vendor/material-design-lite/src/checkbox/checkbox.js
+//= require ../vendor/material-design-lite/src/icon-toggle/icon-toggle.js
+//= require ../vendor/material-design-lite/src/menu/menu.js
+//= require ../vendor/material-design-lite/src/progress/progress.js
+//= require ../vendor/material-design-lite/src/radio/radio.js
+//= require ../vendor/material-design-lite/src/slider/slider.js
+//= require ../vendor/material-design-lite/src/slider/snackbar.js
+//= require ../vendor/material-design-lite/src/spinner/spinner.js
+//= require ../vendor/material-design-lite/src/switch/switch.js
+//= require ../vendor/material-design-lite/src/tabs/tabs.js
+//= require ../vendor/material-design-lite/src/textfield/textfield.js
+//= require ../vendor/material-design-lite/src/tooltip/tooltip.js
 // Complex components (which reuse base components)
-//= require ../vendor/foundation/src/layout/layout.js
-//= require ../vendor/foundation/src/data-table/data-table.js
+//= require ../vendor/material-design-lite/src/layout/layout.js
+//= require ../vendor/material-design-lite/src/data-table/data-table.js
 // And finally, the ripples
-//= require ../vendor/foundation/src/ripple/ripple.js
+//= require ../vendor/material-design-lite/src/ripple/ripple.js
